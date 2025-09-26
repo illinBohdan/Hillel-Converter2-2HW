ee9573bf55340129ac60a4d96023bd3a096df3e6
Bohdan <blazarchuk@gmail.com>
2025-09-26 18:12:03 +0300

    Add ability to convert Miles to Kilometres

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,23 +7,31 @@ public class Main {
     private static final double CONV_KM = 0.621371;
 
     public static void main(String[] args) {
-        System.out.println("App converting miles to kilometres");
+        System.out.println("App converting miles to kilometres and kilometres to miles");
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please enter miles: ");
         double mil = scanner.nextDouble();
 
+        System.out.print("Please enter kilometres: ");
+        double km = scanner.nextDouble();
+
         double kmResult = convectMilesToKms(mil);
+        double milResult = convectKmsToMiles(km);
 
 
         System.out.println("Result ist " + kmResult + " km which is equals " + mil +" miles");
-
+        System.out.println("Result ist " + milResult + " mil which is equals " + km +" km");
 
 
     }
 
     private static double convectMilesToKms(double miles){
         return miles / CONV_KM;
+    }
+
+    private static double convectKmsToMiles(double kilometres){
+        return kilometres * CONV_KM;
     }
 }
